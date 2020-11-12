0eabb4767fa6a14ee2118fb950ca7de8c2d98dd5
rafliedka <rafliedka33@gmail.com>
2020-11-12 12:07:08 +0700

    0110219072_RafliEdka_TI01_Praktikum05

--- praktikum05/js/login.js
+++ praktikum05/js/login.js
@@ -2,7 +2,9 @@ function validasi(){
     var username = document.forms["myForm"]["username"].value;
     var password = document.forms["myForm"]["password"].value;
     if( username == "user" && password == "123"){
-      return true;
+      alert("Login berhasil")
+      document.getElementById("berhasil").innerHTML;
+      return;
     }
     else if(username =="" || password ==""){
       alert("Masukan username dan password Anda");
